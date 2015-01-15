70fa5adbfa128e2ed2e9bae129f009b8b2ca87c5
Pavel Akhrameev <p.akhrameev@gmail.com>
2015-01-16 00:17:05 +0300

    Initial Commit

---
+++ MaxOccursInArray/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  MaxOccursInArray
+//
+//  Created by Pavel Akhrameev on 16.01.15.
+//  Copyright (c) 2015 Pavel Akhrameev. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
